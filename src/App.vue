da9b74b758b4f68b83e0e33788b7c000abcc37fc
Daniel <fomidan@gmail.com>
2025-11-19 10:55:27 +0400

    Текущая дата

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    Текущая дата: {{ currentDate }}
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+        return{
+            currentDate: new Date().toLocaleDateString('ru-RU')
+        }
+        }
+}
+</script>
