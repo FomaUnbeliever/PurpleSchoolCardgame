2141955afc39dfdaffc14d96eeb9716baa542895
Daniel <fomidan@gmail.com>
2025-12-05 11:44:43 +0400

    1 из 10 заданий 4й модуль

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Button from './Button.vue';
+</script>
+
+<template>
+  <main class="main">
+    <Button>Нажми на меня</Button>
+  </main>
+</template>
+
+<style scoped>
+  .main{
+  }
+</style>
---
+++ src/Button.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+<template>
+    <button class="button">
+        <div>
+            <slot name="icon" />
+            <slot name="text" />
+        </div>
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+    .button {
+        border: none;
+        text-rendering: var(--text-color);
+        border-radius: 100px;
+        background: var(--color-bg);
+        padding: 14px;
+        font-family: var(--font);
+        font-size: 20px;
+        font-weight: 600;
+        color: var(--text-color);
+        cursor: pointer;
+ 
+         
+    }
+</style>
